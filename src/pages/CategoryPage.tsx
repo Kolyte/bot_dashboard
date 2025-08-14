0d7d4ccbb30bc393cb277f052b6f625ca2effa2d
Andrii Kolotylo <kolyte@Andriis-MacBook-Air.local>
2025-08-14 09:42:35 +0300

    new commit

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 import { faGear } from '@fortawesome/free-solid-svg-icons'; 
 import { Grid } from "../utils/styles";
-import { Paperclip, Settings } from "react-feather";
+import { Book, BookOpen, Inbox, Paperclip, Settings } from "react-feather";
 export const CategoryPage = () => {
     const {guildId,updateGuildId} = useContext(GuildContext);
     return <div style={{padding:'50px 0'}}>
@@ -30,14 +30,14 @@ export const CategoryPage = () => {
                     <Title>
                         Channel logs
                     </Title>
-                    <Paperclip/>
+                    <BookOpen/>
 
 
                 
                 </Flex>
                 <Grid>
-                    <TextButton>Command Prefix</TextButton>
-                    <TextButton>Welcome Message</TextButton>
+                    <TextButton>X</TextButton>
+                    <TextButton>X</TextButton>
                 </Grid>
             </div>
         </Container>
